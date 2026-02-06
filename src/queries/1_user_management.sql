96dde560a30179cf4758ba78d6afbb97603de15a
freyathomas <freya.thomas@ada.ac.uk>
2026-02-06 13:01:03 +0000

    saving

--- src/queries/1_user_management.sql
+++ src/queries/1_user_management.sql
@@ -2,16 +2,25 @@
 .mode column
 
 -- 1.1
-
+SELECT member_id, first_name, last_name, email, join_date
+FROM members;
 
 -- 1.2
-
+UPDATE members 
+SET phone_number='07000 10005', email='emily.jones.updated@email.com'
+WHERE member_id = 5;
 
 -- 1.3
-
+SELECT COUNT(member_id) AS myCount
+FROM members;
 
 -- 1.4
-
+SELECT member_id, first_name, last_name, registration_count
+FROM members
+WHERE (SELECT MAX(registered) 
+        FROM class_attendance 
+        WHERE( SELECT COUNT(attendance_status)
+             
 
 -- 1.5
 
