6072e54af155057f50d609aecfb73835545fa8cc
freyathomas <freya.thomas@ada.ac.uk>
2026-03-02 11:04:50 +0000

    8

--- src/queries/8_personal_training.sql
+++ src/queries/8_personal_training.sql
@@ -2,4 +2,14 @@
 .mode column
 
 -- 8.1 
+SELECT p.session_id, 
+    m.first_name || ' ' || m.last_name AS member_name, 
+    p.session_date, 
+    p.start_time, 
+    p.end_time
+FROM personal_training_sessions AS p
+JOIN members AS m ON m.member_id = p.member_id
+JOIN staff AS s ON s.staff_id = p.staff_id
+WHERE s.first_name = 'Ivy' AND s.last_name = 'Irwin'
+ORDER BY p.session_date, p.start_time;
 
